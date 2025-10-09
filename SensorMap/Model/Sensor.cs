cecd821a82bbd3e0e11542f73db497099d2ac3c2
user_hp1 <user@HP1>
2025-10-09 19:28:40 +0500

    work with querry to db

--- SensorMap/Model/Sensor.cs
+++ SensorMap/Model/Sensor.cs
@@ -2,6 +2,7 @@
 using ReactiveUI.SourceGenerators;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Reactive.Linq;
 
 namespace SensorMap.Model
 {
@@ -10,11 +11,28 @@ namespace SensorMap.Model
     /// </summary>
     public class Sensor:ReactiveObject
     {
+        private bool _isInitialized = false;
+        private string _name = string.Empty;
+        private SensorType _type;
+        private string _image = string.Empty;
         [Key]
         [Reactive] public int Id { get; set; }
-        [Reactive] public string Name { get; set; } = string.Empty;
-        [Reactive] public SensorType Type { get; set; }
-        [Reactive] public string Image { get; set; } = string.Empty;
+        [Reactive] public string Name 
+        {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+        [Reactive] public SensorType Type
+        {
+            get => _type;
+            set => this.RaiseAndSetIfChanged(ref _type, value);
+        }
+        [Reactive]
+        public string Image
+        {
+            get => _image;
+            set => this.RaiseAndSetIfChanged(ref _image, value);
+        }
 
         private bool _isModified;
         [NotMapped]
@@ -23,23 +41,18 @@ namespace SensorMap.Model
             get => _isModified;
             set => this.RaiseAndSetIfChanged(ref _isModified, value);
         }
-        public Sensor CreateSnapshot()
+        public Sensor()
         {
-            return new Sensor
-            {
-                Id = Id,
-                Name = Name,
-                Type = Type,
-                Image = Image,
-                IsModified = IsModified
-            };
+            SetupPropertyChangeTracking();
         }
-        public void RestoreFromSnapshot(Sensor snapshot)
+        private void SetupPropertyChangeTracking()
         {
-            Name = snapshot.Name;
-            Type = snapshot.Type;
-            Image = snapshot.Image;
-            IsModified = snapshot.IsModified;
+            this.WhenAnyValue(
+                x => x.Name,
+                x => x.Type,
+                x => x.Image)
+                .Skip(1)
+                .Subscribe(_ => IsModified = true);
         }
     }
 }
--- SensorMap/Services/DataBaseProvider.cs
+++ SensorMap/Services/DataBaseProvider.cs
@@ -19,11 +19,29 @@ namespace SensorMap.Services
             _dbContextFactory = dBContextFactory;
         }
 
+        public async Task CreateMechainsm(Mechanism mechanism)
+        {
+            using (AppDBContext dBContext = _dbContextFactory.CreateDbContext())
+            {
+                dBContext.Mechanisms.Add(mechanism);
+                await dBContext.SaveChangesAsync();
+            }
+        }
+
         public async Task CreateSector(Sector sector)
         {
             using (AppDBContext dBContext = _dbContextFactory.CreateDbContext())
             {
                 dBContext.Sectors.Add(sector);
+                await dBContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task CreateSensor(Sensor sensor)
+        {
+            using (AppDBContext dBContext = _dbContextFactory.CreateDbContext())
+            {
+                dBContext.Sensors.Add(sensor);
                 await dBContext.SaveChangesAsync();
             }
         }
@@ -49,6 +67,26 @@ namespace SensorMap.Services
             using (AppDBContext dBContext = _dbContextFactory.CreateDbContext())
             {
                 return await dBContext.Sensors.ToListAsync();
+            }
+        }
+
+        public async Task<T> GetElementByID<T>(int id) where T : class 
+        {
+            try
+            {
+                using (AppDBContext dBContext = _dbContextFactory.CreateDbContext())
+                {
+                    var res = await dBContext.Set<T>().FindAsync(id);
+                    if (res == null)
+                        throw new NullReferenceException();
+                    return res;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Логирование ошибки
+                HandyControl.Controls.MessageBox.Show($"Ошибка при поиске элемента {typeof(T).Name} с ID {id}: {ex.Message}");
+                return null;
             }
         }
     }

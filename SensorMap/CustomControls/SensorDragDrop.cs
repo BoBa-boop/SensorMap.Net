c4641d9d7aaad1ed6dfeed65a209b349748fe255
Nikita Fedoseev <zveri132@gmail.com>
2025-11-11 18:48:24 +0500

    work with SensorCC

--- SensorMap/CustomControls/SensorDragDrop.cs
+++ SensorMap/CustomControls/SensorDragDrop.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.Xml;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -31,7 +32,33 @@ namespace SensorMap.CustomControls
         private bool _isDragging = false;
 
         #region Dependency Properties
+        #region managmentProps
+        public static readonly DependencyProperty CanZoomProperty =
+           DependencyProperty.Register("CanZoom", typeof(bool), typeof(SensorDragDrop), new PropertyMetadata(false));
 
+        public bool CanZoom
+        {
+            get { return (bool)GetValue(CanZoomProperty); }
+            set { SetValue(CanZoomProperty, value); }
+        }
+        public static readonly DependencyProperty CanPanningProperty =
+           DependencyProperty.Register("CanPanning", typeof(bool), typeof(SensorDragDrop),new PropertyMetadata(false));
+
+        public static readonly DependencyProperty ZoomFactorProp =
+           DependencyProperty.Register("ZoomFactor", typeof(double), typeof(SensorDragDrop), new PropertyMetadata(1.1));
+
+        public double ZoomFactor
+        {
+            get { return (double)GetValue(ZoomFactorProp); }
+            set { SetValue(ZoomFactorProp, value); }
+        }
+
+        public bool CanPanning
+        {
+            get { return (bool)GetValue(CanPanningProperty); }
+            set { SetValue(CanPanningProperty, value); }
+        }
+        #endregion
         public static readonly DependencyProperty XProperty =
             DependencyProperty.Register("X", typeof(double), typeof(SensorDragDrop),
                 new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
@@ -61,149 +88,177 @@ namespace SensorMap.CustomControls
             set { SetValue(SensorDropCommandProperty, value); }
         }
 
-        public static readonly DependencyProperty SensorRemoveCommandProperty =
-            DependencyProperty.Register("SensorRemoveCommand", typeof(ICommand), typeof(SensorDragDrop));
-
-        public ICommand SensorRemoveCommand
+        public static readonly DependencyProperty ImageSourceProperty = DependencyProperty.Register("ImageSource", typeof(BitmapFrame), typeof(SensorDragDrop),
+            new PropertyMetadata(default(BitmapFrame)));
+        public BitmapFrame ImageSource
         {
-            get { return (ICommand)GetValue(SensorRemoveCommandProperty); }
-            set { SetValue(SensorRemoveCommandProperty, value); }
+            get { return (BitmapFrame)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
         }
 
-        public static readonly DependencyProperty RemoveSensorNameProperty =
-            DependencyProperty.Register("RemoveSensorName", typeof(string), typeof(SensorDragDrop));
-
-        public string RemoveSensorName
-        {
-            get { return (string)GetValue(RemoveSensorNameProperty); }
-            set { SetValue(RemoveSensorNameProperty, value); }
-        }
         #endregion
-
+        private readonly MatrixTransform _transform = new MatrixTransform();
+        private Point _initialMousePosition;
+        private UIElement _selectedElement;
+        private Vector _draggingDelta;
+        private double _scaleDelta;
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-
+            _scaleDelta = ZoomFactor;
             _canvas = GetTemplateChild("PART_Canvas") as Canvas;
             _sensor = GetTemplateChild("PART_Sensor") as Ellipse;
-
             if (_canvas != null)
             {
-                _canvas.AllowDrop = true;
-                _canvas.DragOver += Canvas_DragOver;
-                _canvas.Drop += Canvas_Drop;
-                _canvas.DragLeave += Canvas_DragLeave;
-            }
-
-            if (_sensor != null)
-            {
-                _sensor.MouseLeftButtonDown += Sensor_MouseLeftButtonDown;
-                _sensor.MouseMove += Sensor_MouseMove;
-                _sensor.MouseLeave += _sensor_MouseLeave;
-                _sensor.MouseEnter += _sensor_MouseEnter;
-                _sensor.MouseLeftButtonUp += Sensor_MouseLeftButtonUp;
-            }
-        }
-
-        private void _sensor_MouseEnter(object sender, MouseEventArgs e)
-        {
-            _sensor.IsHitTestVisible = true;
-            _canvas.IsHitTestVisible = true;
-        }
-
-        private void _sensor_MouseLeave(object sender, MouseEventArgs e)
-        {
-            _sensor.IsHitTestVisible = false;
-            _canvas.IsHitTestVisible = false;
-        }
-
-        private void Sensor_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            _isDragging = false;
-            e.Handled = false; // Важно: позволяем событию всплывать дальше
-        }
-
-        private void Sensor_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                _isDragging = true;
-
-                // Сохраняем текущие координаты перед началом перетаскивания
-                UpdateCoordinates();
-                _sensor.IsHitTestVisible = false;
-                DragDrop.DoDragDrop(_sensor, new DataObject(DataFormats.Serializable, _sensor), DragDropEffects.Move);
-                _isDragging = false;
-                _sensor.IsHitTestVisible = true;
-            }
-
-            e.Handled = false; // Позволяем событию всплывать
-        }
-
-        private void Sensor_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            _isDragging = false;
-            e.Handled = false;
-        }
-
-        private void Canvas_DragOver(object sender, DragEventArgs e)
-        {
-            object data = e.Data.GetData(DataFormats.Serializable);
-
-            if (data is UIElement element)
-            {
-                Point dropPosition = e.GetPosition(_canvas);
-
-                // Центрируем сенсор относительно курсора
-                double left = dropPosition.X - element.RenderSize.Width / 2;
-                double top = dropPosition.Y - element.RenderSize.Height / 2;
-
-                Canvas.SetLeft(element, left);
-                Canvas.SetTop(element, top);
-
-                // Обновляем координаты в реальном времени
-                X = dropPosition.X;
-                Y = dropPosition.Y;
-
-                if (!_canvas.Children.Contains(element))
+                if (_sensor != null)
                 {
-                    _canvas.Children.Add(element);
+                    _sensor.MouseLeftButtonUp += Canvas_Drop;
+                    _sensor.MouseLeftButtonDown += _canvas_MouseMove;
                 }
-
-                e.Handled = true; // Обрабатываем событие здесь
+                _canvas.DragLeave += Canvas_DragLeave;
+                _canvas.MouseMove += _canvas_MouseMove;
+                _canvas.MouseDown += _canvas_MouseDown;
+                _canvas.MouseWheel += _canvas_MouseWheel;//zoom
+                _canvas.MouseUp += _canvas_MouseUp;
             }
         }
 
-        private void Canvas_Drop(object sender, DragEventArgs e)
+        private void _canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            UpdateCoordinates();
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(_canvas); // Границы нашего объекта
+            Size parentSize = RenderSize; // Размер родительского элемента
+            if (e.RightButton == MouseButtonState.Pressed)
+            {
+                Point mousePosition = _transform.Inverse.Transform(e.GetPosition(_canvas));
+                Vector delta = Point.Subtract(mousePosition, _initialMousePosition);
+
+
+
+                //// Получаем координаты нового положения объекта
+                double newX = _transform.Matrix.OffsetX;
+                double newY = _transform.Matrix.OffsetY;
+
+                if (bounds.Width > parentSize.Width)
+                {
+                    newX = _transform.Matrix.OffsetX + delta.X;
+                    if (newX >= 0.0)
+                    {
+                        newX = 0.0;
+                    }
+                    else if (0.0 - newX + parentSize.Width >= bounds.Width)
+                    {
+                        newX = parentSize.Width - bounds.Width;
+                    }
+                }
+                if (bounds.Height > parentSize.Height)
+                {
+                    newY = _transform.Matrix.OffsetY + delta.Y;
+                    if (newY >= 0.0)
+                    {
+                        newY = 0.0;
+                    }
+                    else if (0.0 - newY + parentSize.Height >= bounds.Height)
+                    {
+                        newY = parentSize.Height - bounds.Width;
+                    }
+
+                }
+                var translate = new TranslateTransform(newX, newY);
+                _transform.Matrix = translate.Value; /** _transform.Matrix;*/
+
+                foreach (UIElement child in _canvas.Children)
+                {
+                    child.RenderTransform = _transform;
+                }
+            }
+        
+
+            if (_isDragging && e.LeftButton == MouseButtonState.Pressed)
+            {
+                double x = Mouse.GetPosition(this).X;
+                double y = Mouse.GetPosition(this).Y;
+
+                if (_selectedElement != null)
+                {
+                    Canvas.SetLeft(_selectedElement, x + _draggingDelta.X);
+                    Canvas.SetTop(_selectedElement, y + _draggingDelta.Y);
+                }
+            }
+        }
+
+        private void _canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _isDragging = false;
+            _selectedElement = null;
+        }
+
+        private void _canvas_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double scaleFactor = ZoomFactor;
+            if (e.Delta < 0)
+            {
+                scaleFactor = 1.0 / scaleFactor;
+            }
+
+            Point mousePostion = e.GetPosition(this);
+
+            Matrix scaleMatrix = _transform.Matrix;
+            scaleMatrix.ScaleAt(scaleFactor, scaleFactor, mousePostion.X, mousePostion.Y);
+            _scaleDelta = _scaleDelta * scaleFactor;
+            _transform.Matrix = scaleMatrix;
+
+            foreach (UIElement child in _canvas.Children)
+            {
+                double x = Canvas.GetLeft(child);
+                double y = Canvas.GetTop(child);
+
+                double sx = Math.Round(x * scaleFactor,2);
+                double sy = Math.Round(y * scaleFactor,2);
+
+                Canvas.SetLeft(child, sx);
+                Canvas.SetTop(child, sy);
+
+                child.RenderTransform = _transform;
+            }
+        }
+
+        private void _canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Right)
+            {
+                _initialMousePosition = _transform.Inverse.Transform(e.GetPosition(this));
+            }
+
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                //перемещаем только объект
+                if (_canvas.Children.Contains((UIElement)e.Source))
+                {
+                    _selectedElement = (UIElement)e.Source;
+                    Point mousePosition = Mouse.GetPosition(this);
+                    double x = Canvas.GetLeft(_selectedElement);
+                    double y = Canvas.GetTop(_selectedElement);
+                    Point elementPosition = new Point(x, y);
+                    _draggingDelta = elementPosition - mousePosition;
+                }
+                _isDragging = true;
+            }
+        }
+
+        private void Canvas_Drop(object sender, MouseButtonEventArgs e)
+        {
+            X = Math.Round(Canvas.GetLeft(_selectedElement) / _scaleDelta,1);
+            Y = Math.Round(Canvas.GetTop(_selectedElement) / _scaleDelta, 1);
             SensorDropCommand?.Execute(null);
             e.Handled = true;
         }
 
         private void Canvas_DragLeave(object sender, DragEventArgs e)
         {
-            object data = e.Data.GetData(DataFormats.Serializable);
-
-            if (data is FrameworkElement element)
-            {
-                _canvas.Children.Remove(element);
-                RemoveSensorName = element.Name;
-                SensorRemoveCommand?.Execute(null);
-            }
-
-            e.Handled = true;
+           
         }
 
-        private void UpdateCoordinates()
-        {
-            if (_sensor != null && _canvas != null)
-            {
-                Point position = _sensor.TranslatePoint(new Point(_sensor.ActualWidth / 2, _sensor.ActualHeight / 2), _canvas);
-                X = position.X;
-                Y = position.Y;
-            }
-        }
+        
 
         // Метод для обновления позиции сенсора извне
         public void SetPosition(double x, double y)
@@ -212,7 +267,6 @@ namespace SensorMap.CustomControls
             {
                 Canvas.SetLeft(_sensor, x - _sensor.ActualWidth / 2);
                 Canvas.SetTop(_sensor, y - _sensor.ActualHeight / 2);
-                UpdateCoordinates();
             }
         }
     }

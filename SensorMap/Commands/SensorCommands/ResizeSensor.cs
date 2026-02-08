aed5359c48d54cfeb8a303bb62924bceb0928654
Nikita Fedoseev <zveri132@gmail.com>
2026-02-09 01:26:42 +0500

    899

---
+++ SensorMap/Commands/SensorCommands/ResizeSensor.cs
@@ -0,0 +1,22 @@
+﻿using SensorMap.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SensorMap.Commands.SensorCommands
+{
+    public class ResizeSensor : IUndoRedoCommand
+    {
+        public void Do()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

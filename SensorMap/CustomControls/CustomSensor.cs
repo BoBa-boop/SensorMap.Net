8d1bf42cae9378d4b1300b1fcd856a044d816108
user_hp1 <user@HP1>
2026-06-30 08:56:48 +0500

    Подписка на изменение фона датчика убрана для копий. Убрал клонирование перед State=added, тк это вызывало добавление на карту лишнего датчика.

--- SensorMap/CustomControls/CustomSensor.cs
+++ SensorMap/CustomControls/CustomSensor.cs
@@ -204,7 +204,7 @@ namespace SensorMap.CustomControls
                 _sensorSubscription = SensorData.WhenAnyValue(x => x.Sensor)
                     .Subscribe(_ =>
                     {
-                        if (SensorData?.Sensor?.SensorType?.Color != null && SensorData.IsNew == false)
+                        if (SensorData?.Sensor?.SensorType?.Color != null && SensorData.Description != "Копия")
                         {
                             CustomBackground = (SolidColorBrush)new BrushConverter().ConvertFrom(SensorData.Sensor.SensorType.Color);
                         }
